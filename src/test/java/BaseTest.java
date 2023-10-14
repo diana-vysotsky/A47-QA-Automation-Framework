b7946213a6465b391a6604f0c760adea8504b166
diana-vysotsky <137337906+diana-vysotsky@users.noreply.github.com>
2023-10-14 17:58:24 -0500

    Esri

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,10 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
 
 
 import java.time.Duration;
@@ -22,35 +22,58 @@ public class BaseTest {
     public static String url = "https://www.arcgis.com/";
 
     @BeforeSuite
+    //Set up the ChromeDriver using WebDriverManager
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeMethod
-
+    // Set up ChromeDriver and open the browser
     public void launchBrowser(){
         //Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
+
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.manage().window().maximize();
         driver.get(url);
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
     @AfterMethod
+    // Close the browser after the test
     public void closeBrowser(){
         driver.quit();
     }
 
+    //Method to sign in to the ArcGIS app
+    public static void signIn(){
+        clickSignIn();
+        provideEmail("vysotskydiana@gmail.com");
+        providePassword("TestArcGIS2023");
+        clickSubmit();
+    }
+
+    //Helper methods
+
+    //Method to open Navigation Map
+    public static void openNavigationMap(){
+        clickMapMenuHeader();
+        clickOpenInMapViewerButton();
+        clickBaseMapMenuButton();
+        clickNavigationMap();
+    }
+
+    //Click Sign In button to open sign in page
     public static void clickSignIn(){
         WebElement signInButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.esri-header-account-control")));
         signInButton.click();
     }
 
+    //Enter email to the username field
     public static void provideEmail(String email){
         WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='username']")));
 
@@ -59,17 +82,62 @@ public class BaseTest {
         emailField.sendKeys(email);
     }
 
+    //Enter password to the password field
     public static void providePassword(String password){
         WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='password']")));
+
         passwordField.click();
         passwordField.clear();
         passwordField.sendKeys(password);
     }
 
+    //Click Submit button to sign in
     public static void clickSubmit(){
         WebElement submitButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
         submitButton.click();
     }
+
+    //Click Map on the menu header
+    public static void clickMapMenuHeader(){
+        WebElement mapMenuHeader = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a#esri-header-menus-link-desktop-0-3")));
+        mapMenuHeader.click();
+    }
+
+    //Click Open in Map Viewer button
+    public static void clickOpenInMapViewerButton(){
+        WebElement OpenInMapViewerButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a#esri-header-menus-link-desktop-1-0")));
+        OpenInMapViewerButton.click();
+    }
+
+    //Click Basemap menu button
+    public static void clickBaseMapMenuButton(){
+        WebElement BaseMapMenuButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span#webmap-basemap_label")));
+        BaseMapMenuButton.click();
+    }
+
+    //Choose Navigation map from the Base map menu
+    public static void clickNavigationMap(){
+        WebElement BaseMapMenuOptions = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#basemap-gallery")));
+
+        WebElement navigationMap = BaseMapMenuOptions.findElement(By.cssSelector("#galleryNode_61ffcf610f314933916e4b2c0e477b29 > a > img"));
+        navigationMap.click();
+    }
+
+    //Find zoom in button and click it
+    public static void zoomIn() {
+        WebElement zoomInButton = driver.findElement(By.cssSelector("div.esriSimpleSliderIncrementButton"));
+        wait.until(ExpectedConditions.elementToBeClickable(zoomInButton)).click();
+    }
+
+    //Find zoom out button and click it
+    public static void zoomOut() {
+        WebElement zoomOutButton = driver.findElement(By.cssSelector("div.esriSimpleSliderDecrementButton"));
+        wait.until(ExpectedConditions.elementToBeClickable(zoomOutButton)).click();
+    }
+
+
+
+
 
 
 }
--- src/test/java/MapTests.java
+++ src/test/java/MapTests.java
@@ -9,12 +9,10 @@ import org.testng.annotations.Test;
 public class MapTests extends BaseTest{
     @Test
 
-    public void signIn() throws InterruptedException{
-        clickSignIn();
-        provideEmail("vysotskydiana@gmail.com");
-        providePassword("TestArcGIS2023");
-        clickSubmit();
+    public void testSignInValidEmailPassword() throws InterruptedException{
+        signIn();
 
+        //Verify that sign in was successful and account menu button is visible
         WebElement accountMenuButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button#esri-header-account-control")));
         Assert.assertTrue(accountMenuButton.isDisplayed());
     }
@@ -22,91 +20,47 @@ public class MapTests extends BaseTest{
 
     public void openMap() throws InterruptedException{
         signIn();
-        WebElement mapMenuHeader = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a#esri-header-menus-link-desktop-0-3")));
-        mapMenuHeader.click();
 
-        WebElement OpenInMapViewerButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a#esri-header-menus-link-desktop-1-0")));
-        OpenInMapViewerButton.click();
+        openNavigationMap();
 
-        WebElement BaseMapMenuButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span#webmap-basemap_label")));
-        BaseMapMenuButton.click();
-
-        WebElement BaseMapMenuOptions = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#basemap-gallery")));
-
-        WebElement navigationMap = BaseMapMenuOptions.findElement(By.cssSelector("#galleryNode_61ffcf610f314933916e4b2c0e477b29 > a > img"));
-        navigationMap.click();
+        //Verify the map is open and title MY Map is visible
+        String pageTitle = driver.getTitle();
+        Assert.assertTrue(pageTitle.contains("My Map"));
     }
     @Test
     public void zoomMap() throws InterruptedException{
-        openMap();
+        signIn();
 
-        WebElement zoomInButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.esriSimpleSliderIncrementButton")));
-
-            zoomInButton.click();
-
-
-        WebElement zoomOutButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.esriSimpleSliderDecrementButton")));
-
-            zoomOutButton.click();
-
+        openNavigationMap();
 
         WebElement scaleBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.esriScalebarLabel.esriScalebarFirstNumber")));
-        String initialScale = getScaleValue(scaleBar);
 
-        zoomInButton.click();
+        // Get the initial scale bar text
+        String initialScaleBarText = scaleBar.getText();
 
+        zoomIn();
 
-        String updatedScale = getScaleValue(scaleBar);
+        wait.until(ExpectedConditions.not(ExpectedConditions.textToBePresentInElement(scaleBar, initialScaleBarText)));
 
-        Assert.assertNotEquals(initialScale, updatedScale);
+        String zoomedInScaleBarText = scaleBar.getText();
 
-      }
+        Assert.assertNotEquals(initialScaleBarText, zoomedInScaleBarText);
+
+        zoomOut();
+
+        wait.until(ExpectedConditions.not(ExpectedConditions.textToBePresentInElement(scaleBar, zoomedInScaleBarText)));
+
+        String zoomedOutScaleBarText = scaleBar.getText();
+
+        Assert.assertNotEquals(zoomedInScaleBarText, zoomedOutScaleBarText);
+        
+    }
 
       @Test
       public void testCityDistanceMeasurement() throws InterruptedException {
-          openMap();
 
-          WebElement measureTool = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#webmap-measure_label"))); // Replace with the actual element selector
-          measureTool.click();
-          WebElement measureOptions = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#measureLabel")));
-          WebElement distanceTool = measureOptions.findElement(By.cssSelector("#dijit_form_ToggleButton_1 > span.dijitReset.dijitInline.dijitIcon.distanceIcon"));
-
-          Actions actions = new Actions(driver);
-
-          // Click and hold the measuring tool
-          actions.clickAndHold(distanceTool).perform();
-
-          // Move to the first point on the map (e.g., coordinates)
-          WebElement point1 = driver.findElement(By.id("point1")); // Replace with the actual element selector
-          actions.moveToElement(point1).perform();
-
-          // Release the mouse to set the first point
-          actions.release().perform();
-
-          // Move to the second point on the map (e.g., coordinates)
-          WebElement point2 = driver.findElement(By.id("point2")); // Replace with the actual element selector
-          actions.moveToElement(point2).perform();
-
-          // Release the mouse to set the second point
-          actions.release().perform();
-
-          // Get the distance displayed on the screen
-          WebElement distanceLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#dijit_layout_ContentPane_4"))); // Replace with the actual element selector
-          String distance = distanceLabel.getText();
-
-          // Expected distance (replace with the actual expected value)
-          String expectedDistance = "585.1 Kilometers";
-
-          // Assert that the measured distance matches the expected distance
-          Assert.assertEquals(distance, expectedDistance, "Measured distance doesn't match the expected distance");
 
       }
 
-
-
-    private String getScaleValue(WebElement scaleBar) {
-        return scaleBar.getText();
-    }
-
-    }
+}
 
